e939c6f7bfc7104308ad01f1a343d57ef25fda21
yahav <yahavigal@gmail.com>
2018-08-19 13:56:04 +0300

    updated tests figure8 and hover to handle variable number of drones

---
+++ crazyflie_demo/scripts/tests/hover.py
@@ -0,0 +1,23 @@
+import sys, time
+from Simulator import crazyflie, rospy, tf
+
+
+def hover(drones):
+	rospy.init_node('hover')
+	print "starting"
+	listener = tf.TransformListener()
+	cfs = []
+	for drone_name in drones:
+		cf = crazyflie.Crazyflie(drone_name, drone_name)
+		cfs.append(cf)
+		cf.setParam("commander/enHighLevel", 1)
+		(real_x, real_y, real_z), rot = listener.lookupTransform("/world", "/{}".format(drone_name), rospy.Time(0))
+		print "Takeoff from ({:.2f} , {:.2f} , {:.2f})".format(real_x, real_y, real_z)
+		cf.takeoff(targetHeight=0.5, duration=2.5)
+		time.sleep(2.5)
+	time.sleep(120)
+	for cf in cfs:
+		cf.land(targetHeight=0.0, duration=2.0)
+
+
+hover(sys.argv[1:])
