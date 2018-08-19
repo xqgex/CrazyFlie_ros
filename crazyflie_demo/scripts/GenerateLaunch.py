05d8823a0c1e59eb50691bc7e8ffc32dd3feda5b
xqgex <xqgex.n@gmail.com>
2018-08-19 12:40:34 +0300

    Clean launch folder

--- crazyflie_demo/scripts/GenerateLaunch.py
+++ crazyflie_demo/scripts/GenerateLaunch.py
@@ -3,10 +3,6 @@
 import logger, sys
 cf_logger = logger.get_logger(__name__) # debug(), info(), warning(), error(), exception(), critical()
 
-FILE_NAME = "../launch/crazy_game.launch"
-IP = "172.16.1.1"
-PORT = "3883"
-
 def droneNode(current_drone_number, drones_count):
 	if current_drone_number+1 == drones_count:
 		trackers = "      trackers:\n"
@@ -48,7 +44,7 @@ def droneNode(current_drone_number, drones_count):
     </node>
   </group>""".format(num=current_drone_number, trackers=trackers)
 
-def fileStracture(arguments):
+def fileStracture(ip, port, arguments):
 	drone_args = []
 	for drone_number in range(len(arguments)):
 		drone_args.append("""  <arg name="uri{num}" default="radio://0/80/2M/E7E7E7E70{mac}" />
@@ -64,22 +60,17 @@ def fileStracture(arguments):
   <include file="$(find crazyflie_driver)/launch/crazyflie_server.launch"></include>
 {drones_text}
 </launch>
-""".format(ip=IP, port=PORT, drone_args=drone_args, drones_text=drones_text)
+""".format(ip=ip, port=port, drone_args=drone_args, drones_text=drones_text)
 
-def main(arguments):
-	if (len(arguments) == 0) or (4 < len(arguments)):
-		cf_logger.error("Invalid call, Usage:")
-		cf_logger.error("python GenerateLaunch.py {[crazyflie0] ..}")
-		cf_logger.error("For example:")
-		cf_logger.error("python GenerateLaunch.py crazyflie2 crazyflie3")
-	else:
-		with open(FILE_NAME, "w") as outfile:
-			outfile.write(fileStracture(arguments))
-		cf_logger.info("File {} created successfully".format(FILE_NAME))
+def generate(file_name, ip, port, arguments):
+	try:
+		with open(file_name, "w") as outfile:
+			outfile.write(fileStracture(ip, port, arguments))
+		return True
+	except exception as e:
+		cf_logger.exception("Failed to create {}".format(file_name))
+		return False
 
 if __name__ == "__main__":
-	cf_logger.info("######################################################")
-	cf_logger.info("####                   Started                    ####")
-	cf_logger.info("######################################################")
-	main(sys.argv[1:])
+	print "This is not the way to do it..."
 
