32deeb0192e779cc6ef13dd529e4200fb36d1a50
CrazyGame <matan.harel.mh@gmail.com>
2018-08-19 15:39:50 +0200

    Move untested tests

---
+++ crazyflie_demo/scripts/tests_untested/figure8.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import sys, time, uav_trajectory
+import crazyflie, rospy
+
+
+def figure8(drones):
+	rospy.init_node('figure8')
+	cfs = [crazyflie.Crazyflie(drone_name, drone_name) for drone_name in drones]
+	for cf in cfs:
+		cf.setParam("commander/enHighLevel", 1)
+	print("starting")
+	traj1 = uav_trajectory.Trajectory()
+	traj1.loadcsv("./tests/takeoff.csv")
+	traj2 = uav_trajectory.Trajectory()
+	traj2.loadcsv("./tests/figure8.csv")
+	print(traj1.duration)
+	for cf in cfs:
+		cf.uploadTrajectory(0, 0, traj1)
+		cf.uploadTrajectory(1, len(traj1.polynomials), traj2)
+		cf.startTrajectory(0, timescale=1.0)
+	time.sleep(traj1.duration * 2.0)
+	while True:
+		for cf in cfs:
+			cf.startTrajectory(1, timescale=2.0)
+		time.sleep(traj2.duration * 2.0)
+	for cf in cfs:
+		cf.startTrajectory(0, timescale=1.0, reverse=True)
+	time.sleep(traj1.duration * 1.0)
+	for cf in cfs:
+		cf.stop()
+
+
+figure8(sys.argv[1:])
