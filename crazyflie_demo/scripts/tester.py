c0946ca1c703dbf275acc79fe1ab1743d71db084
CrazyGame <matan.harel.mh@gmail.com>
2018-08-20 12:50:54 +0200

    Remove main3 from test4er

--- crazyflie_demo/scripts/tester.py
+++ crazyflie_demo/scripts/tester.py
@@ -5,7 +5,6 @@ import crazyflie, rospy, uav_trajectory
 from tests import *
 
 CF_LIST = []
-CF_NAMES = ["crazyflie2"]
 
 def signal_handler(sig, frame):
 	try:
@@ -21,31 +20,6 @@ def stopAllCrazyFlies():
 		print "{} forced landing".format(cf)
 		time.sleep(1.5)
 		cf.stop()
-
-def main3():
-	signal.signal(signal.SIGINT, signal_handler)
-	global CF_LIST
-	rospy.init_node('the_new_gofetch')
-	cf = crazyflie.Crazyflie(CF_NAMES[0], CF_NAMES[0])
-	cf.setParam("commander/enHighLevel", 1)
-	CF_LIST.append(cf)
-	print "Starting"
-	traj1 = uav_trajectory.Trajectory()
-	traj1.loadcsv("./tests/takeoff.csv")
-	traj2 = uav_trajectory.Trajectory()
-	traj2.loadcsv("./tests/figure8.csv")
-	print(traj1.duration)
-	cf.uploadTrajectory(0, 0, traj1)
-	cf.uploadTrajectory(1, len(traj1.polynomials), traj2)
-	cf.startTrajectory(0, timescale=1.0)
-	time.sleep(traj1.duration * 2.0)
-	while True:
-		cf.startTrajectory(1, timescale=2.0)
-		time.sleep(traj2.duration * 2.0)
-	cf.startTrajectory(0, timescale=1.0, reverse=True)
-	time.sleep(traj1.duration * 1.0)
-	cf.stop()
-	print "Done"
 
 def main():
 	signal.signal(signal.SIGINT, signal_handler)
